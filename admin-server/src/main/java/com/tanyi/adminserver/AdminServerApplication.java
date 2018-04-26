d892ee0e57bb19d9faa4950adc033cf046cb21ed
745819123 <tan.yi1993@163.com>
2018-04-27 00:46:43 +0800

    init

--- admin-server/src/main/java/com/tanyi/adminserver/AdminServerApplication.java
+++ admin-server/src/main/java/com/tanyi/adminserver/AdminServerApplication.java
@@ -1,14 +1,16 @@
-package com.tanyi.auth;
+package com.tanyi.adminserver;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
+@EnableAdminServer
 @SpringBootApplication
-public class AuthApplication {
+public class AdminServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthApplication.class, args);
+		SpringApplication.run(AdminServerApplication.class, args);
 	}
 }
